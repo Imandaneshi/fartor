1adcc7221d3e8b6a0b7bee6ba9ac34e15953b4e4
Iman Daneshi <emandaneshikohan@gmail.com>
2020-05-03 04:10:57 +0430

    feat(codebase): Add codebase

---
+++ README.md
@@ -0,0 +1,2 @@
+# fartor
+An imaging platform
---
+++ wait_for_db.sh
@@ -0,0 +1,1 @@
+#!/bin/bashRETRIES=20until PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."  sleep 1done
